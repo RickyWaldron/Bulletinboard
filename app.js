97c4cd8dac09da65718766dd4666aac09ebe472c
Ricky Waldron <ricky_waldron@hotmail.com>
2017-11-29 11:01:01 +0100

    TableInststeadOfList

--- app.js
+++ app.js
@@ -12,11 +12,11 @@ const pg = require('pg')
 const Client = pg.Client
 
 const client = new Client({
-	user: process.env.user,
-	host: 'localhost',
-	database: process.env.database,
-	password: process.env.password,
-	port: 5432,
+    user: process.env.user,
+    host: 'localhost',
+    database: process.env.database,
+    password: process.env.password,
+    port: 5432,
 })
 
 client.connect()
@@ -24,60 +24,63 @@ app.use(bodyParser.urlencoded())
 app.set('view engine', 'pug')
 
 app.get('/addMessages', (req, res) => {
-	res.render('addMessages')
+    res.render('addMessages')
 })
 
 
 
 app.post('/addMessages', (req, res) => {
-	let userName = req.body.name
-	let theTitle = req.body.title
-	let theMessage = req.body.bodyMessage
-	const query = {
-		text: (`SELECT * FROM users WHERE name = '${userName}'`)
-	}
-	const query2 = {
-		text: (`INSERT INTO users (name)
-				VALUES ('${userName}') RETURNING *`) 
-	}
-	const query3 = {
-		 text: (`INSERT INTO messages (title, body, userid) 
+    let userName = req.body.name
+    let theTitle = req.body.title
+    let theMessage = req.body.bodyMessage
+    const query = {
+        text: (`SELECT * FROM users WHERE name = '${userName}'`)
+    }
+    const query2 = {
+        text: (`INSERT INTO users (name)
+				VALUES ('${userName}') RETURNING *`)
+    }
+    const query3 = {
+        text: (`INSERT INTO messages (title, body, userid) 
 		 		VALUES ('${theTitle}', '${theMessage}', (
 		 		SELECT (userid) FROM users WHERE name= '${userName}')) RETURNING *`)
-		}		
+    }
 
-	client.query(query, (err, result) => {
-		console.log(result.rows.length)
-			if (result.rows.length == 0){
-				return client.query(query2, (error, result2) => {
-						return client.query(query3, (err, result3) => {
-							res.render('addMessages')
-						})
-					})
-				}		
-			return client.query(query3, (err, result3) => {
-				res.render('addMessages')
-				})
-			})
-		})		
+    client.query(query, (err, result) => {
+        if (err) throw err
+        console.log(result.rows.length)
+        if (result.rows.length == 0) {
+            return client.query(query2, (error, result2) => {
+                return client.query(query3, (err, result3) => {
+                    res.render('addMessages')
+                })
+            })
+        }
+        return client.query(query3, (err, result3) => {
+            if (err) throw err
+            res.render('addMessages')
+        })
+    })
+})
 
 
 app.get('/allMessages', (req, res) => {
-	
-	const query = { 
-		text: "SELECT * FROM messages" 
-	}
 
-	client.query(query, (err, result) => {
-		let data = []
-		data.push(result)
-	res.render('allMessages', {data: data})		
-	})
+    const query = {
+        text: "SELECT * FROM messages"
+    }
+
+    client.query(query, (err, result) => {
+        if (err) throw (err)
+        let data = []
+        data.push(result)
+        res.render('allMessages', { data: data })
+    })
 
 })
 
 // client.end()
 
-app.listen(3000, ()=> {
-	console.log("listening")
+app.listen(3000, () => {
+    console.log("listening")
 })
